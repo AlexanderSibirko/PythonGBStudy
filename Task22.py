59f70a4d821f9dd1db6b4a733963ee18610bc0a6
Alexander <Alexandersibirko@yandex.ru>
2022-02-05 01:44:25 +0300

    solved with cuts

--- Task22.py
+++ Task22.py
@@ -8,12 +8,16 @@ def sum_odd(list_):
 
 list_ = [0,1,2,3,4,5,6,7,8,9]
 print(sum_odd(list_))
+print(sum(list_[::2]))
 
 list_ = [15]
 print(sum_odd(list_))
+print(sum(list_[::2]))
 
 list_ = [5,5,5]
 print(sum_odd(list_))
+print(sum(list_[::2]))
 
 list_ = []
 print(sum_odd(list_))
+print(sum(list_[::2]))
