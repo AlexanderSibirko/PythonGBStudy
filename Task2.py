60b699be281bd20782c419eb3ff82c674657e0e8
Alexander <Alexandersibirko@yandex.ru>
2022-02-04 23:05:35 +0300

    trying list comprehension

--- Task2.py
+++ Task2.py
@@ -1,21 +1,25 @@
 from GeneralMods import MyMethods as MM
 
 # 2. Найти максимальной из пяти чисел
-numbers_list = []
+
 
 print('Enter 5 numbers:')
-for i in range(0,5):
-    numbers_list.append(MM.input_int_number())
-    
-print(numbers_list)
+numbers_list = [MM.input_int_number() for i in range(0,5)]
+# print(numbers_list)
+print(max(numbers_list))
 
-def max_number(values_array):
-    return max(values_array)
+# for i in range(0,5):
+#     numbers_list.append(MM.input_int_number())
+
+
+# print(numbers_list)
+
+# def max_number(values_array):
+#     return max(values_array)
        
-print(max_number(numbers_list))
 
-values_set = {1,646,345,23,24}
-values_tuple = (35,24,634,324,3436)
+# values_set = {1,646,345,23,24}
+# values_tuple = (35,24,634,324,3436)
 
-print(max_number(values_set))
-print(max_number(values_tuple))
+# print(max_number(values_set))
+# print(max_number(values_tuple))
