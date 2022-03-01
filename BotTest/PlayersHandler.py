3d28cfb400872f4709d4f902754ef43cb05362be
Alexander Sibirko <Alexandersibirko@yandex.ru>
2022-03-01 17:49:50 +0300

    started working on PlayerHandler

--- BotTest/PlayersHandler.py
+++ BotTest/PlayersHandler.py
@@ -1,12 +1,21 @@
 
 # list = [telegram_id, telegram_id...]
 players = []
-# players_data = {'telegram_id': {'Name': string, 'status': integer, 'game_idx': integer}
-players_data = ({})
+# players_data = {'telegram_id': {'Name': string, games_id': {1,2,5,...}}
+players_data = {}
+
+def get_players():
+    return players
+
+def add_player_data(key, data, player_id):
+    players_data[player_id][key] = data
+
+def get_player_data(key, player_id):
+    return players_data[player_id][key]
 
 def add_player(user_id, user_name):
     players.append(user_id)
-    players_data[user_id] = {'Name': user_name, 'status': 0}
+    add_player_data('Name', user_name, user_id)
 
 def remove_player(user_id):
     if user_id in players:
@@ -14,7 +23,7 @@ def remove_player(user_id):
             players.remove(user_id)
             return True # player was removed
         else:
-            # ToDo: if status = 1, inform opponent have left the game
+            # ToDo: if status = 1, inform opponent have left thd_player
             players.remove(user_id)
             return True # player was removed
     return False # player wasn't removed
